2a702f14fe3cd1f64247d1464e930c2d77af1876
nurshik <nurshik1@gmail.com>
2021-06-13 11:23:53 -0500

    Solar Syatem Structure is ready to deploy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-
+        SolarSystem FactorX =new SolarSystem();
+        FactorX.Sun=1;
+        FactorX.Moon=3;
+        FactorX.Planets=2;
     }
 }
